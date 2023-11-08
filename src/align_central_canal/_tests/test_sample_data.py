ecc70e9200bae901440732e57a9b8203a6c8948b
daggermaster3000 <quillan.favey@gmail.com>
2023-11-08 13:30:11 +0100

    initial commit

---
+++ src/align_central_canal/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from align_central_canal import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ src/align_central_canal/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from align_central_canal import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
